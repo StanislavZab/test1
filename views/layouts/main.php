f4555807249045e03067cdacf8080f74dedf408b
StanislavZab <hrustik-st@bk.ru>
2019-10-19 21:47:57 +0500

    Bootstrap

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,8 +1,28 @@
+<?php $this->beginPage(); ?>
 <html>
     <head>
         <title>Test YII2</title>
+        <?php $this->head(); ?>
     </head>
     <body>
-        <?= $content ?>
+    <?php $this->beginBody(); ?>
+<?php
+    \yii\bootstrap\NavBar::begin([
+        'brandLabel' => 'ITVDN',
+        'brandUrl' => Yii::$app->homeUrl,
+        'options' => [
+                'class' => 'navbar-default navbar-fixed-top'
+        ]
+    ]);
+    \yii\bootstrap\NavBar::end();
+
+
+?>
+        <div class="container" style="margin-top: 70px">
+            <?= $content ?>
+        </div>
+        <?php $this->endBody(); ?>
     </body>
 </html>
+<?php $this->endPage(); ?>
+
